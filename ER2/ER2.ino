5db14c46022cedebd229944b7ce4bbc43b4acf14
hengtramit <tramit.heng@student.cadt.edu.kh>
2023-06-22 15:36:21 +0700

    Update ER2.ino

--- ER2/ER2.ino
+++ ER2/ER2.ino
@@ -4,12 +4,13 @@
 #include <Servo.h>
 
 #define debug Serial
-#define home_ring 48
-#define minPush 49
-#define maxPush 13
-#define ina 46
-#define inb 47
-#define escpin 6
+#define home_ring 48 //container sensor pin
+#define minPush 49 //ring pusher sensor pin
+#define maxPush 12 //ring pusher sensor pin
+#define ina 9 //driver pin
+#define inb 10 //driver pin
+#define escpin 6 //cannon esc pin
+//wheel motors pins
 #define pwm1 3
 #define pwm2 2
 #define pwm3 5
@@ -18,19 +19,20 @@
 #define dir2 26
 #define dir3 29
 #define dir4 32
+//container steppers pin: 41, 42
 
 const int Mpush_ring_Min = 1600;
 const int Mpush_ring_Max = 700;
 const int minspeed = 100;
 const int maxspeed = 140;
 
-int canonSpeed = 1000;
+int canonSpeed = 1185;
 int buttonA = 282;
-
-int buttonB;
-int buttonG;
+int buttonB = 282;
+int buttonC;
 int timeReload = 0;
 int canonMode;
+int containerMode;
 
 Servo Mpush_ring, esc;
 AccelStepper Mpull_ring(1, 41, 42);
@@ -66,11 +68,11 @@ void setup() {
   esc.attach(escpin, 1000, 1500);
   Mpull_ring.setMaxSpeed(3000);
   Mpush_ring.writeMicroseconds(Mpush_ring_angle);
-  esc.writeMicroseconds(900);
+  esc.writeMicroseconds(1050);
   sbus_rx.Begin();
   delay(5000);
   Movehome_ring();
-  
+
   channels = "";
   if (sbus_rx.Read()) {
     data = sbus_rx.data();
@@ -78,8 +80,8 @@ void setup() {
       channels = channels + String(data.ch[i]) + ",";
     }
     buttonA = data.ch[8];
-    buttonB = 0;
-    buttonG = 3;
+    buttonC = 0;
+    buttonB = data.ch[9];
   }
 }
 
@@ -125,22 +127,16 @@ void loop() {
       }
     }
     CanonSpeed();
-    if (data.ch[8] != buttonA) {
+
+    //shoot
+    if (data.ch[8] != buttonA && containerMode == 1) {
       shoot();
       reload();
       buttonA = data.ch[8];
     }
 
     //container
-    if (data.ch[9] != 1002) {
-      buttonB = 1;
-      Movehome_ring();
-    }
-    if (data.ch[9] == 1002 && buttonB == 1) {
-      pick_up(5350);
-      reload();
-      buttonB = 0;
-    }
+    container();
   }
   if (debug.available() > 0) {
 
@@ -188,8 +184,8 @@ void Movehome_ring() {
       Mpull_ring.runSpeed();
     }
   }
+  timeReload = 0;
   Mpull_ring.stop();
-  
 }
 
 void plat(boolean stat) {
@@ -222,19 +218,21 @@ void reload() {
   if (timeReload < 9) {
     pick_up(350);
     timeReload++;
+  } else if (timeReload == 9) {
+    timeReload++;
   } else {
-    Movehome_ring();
     timeReload = 0;
+    Movehome_ring();
   }
 }
 
 void Mpush(int dir) {
   if (dir == 0) {
-    analogWrite(ina, 255);
+    analogWrite(ina, 100);
     analogWrite(inb, 0);
   } else if (dir == 1) {
     analogWrite(ina, 0);
-    analogWrite(inb, 255);
+    analogWrite(inb, 150);
   } else {
     analogWrite(ina, 0);
     analogWrite(inb, 0);
@@ -245,6 +243,7 @@ void shoot() {
   while (digitalRead(maxPush) == 1) {
     Mpush(1);
   }
+  // delay(300);
   while (digitalRead(minPush) == 1) {
     Mpush(0);
   }
@@ -252,68 +251,117 @@ void shoot() {
 }
 
 void wheel(String strcmd, int velo, long tim) {
+  int offSet1;
+  int offSet2;
+  int offSet3;
+  int offSet4;
+
   if (strcmd.indexOf("sto") > -1) {
-    accMove(velo, tim);
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("fw") > -1) {
     digitalWrite(dir1, 1);
     digitalWrite(dir2, 1);
     digitalWrite(dir3, 0);
     digitalWrite(dir4, 0);
-    accMove(velo, tim);
+    // offSet1 = 7;
+    // offSet2 = 0;
+    // offSet3 = 10;
+    // offSet4 = 13;
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("rv") > -1) {
     digitalWrite(dir1, 0);
     digitalWrite(dir2, 0);
     digitalWrite(dir3, 1);
     digitalWrite(dir4, 1);
-    accMove(velo, tim);
+    // offSet1 = -10;
+    // offSet2 = -10;
+    // offSet3 = 0;
+    // offSet4 = 30;
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("lf") > -1) {
     digitalWrite(dir1, 0);
     digitalWrite(dir2, 1);
     digitalWrite(dir3, 1);
     digitalWrite(dir4, 0);
-    accMove(velo, tim);
+    // offSet1 = 13;
+    // offSet2 = 0;
+    // offSet3 = 10;
+    // offSet4 = 0;
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("rg") > -1) {
     digitalWrite(dir1, 1);
     digitalWrite(dir2, 0);
     digitalWrite(dir3, 0);
     digitalWrite(dir4, 1);
-    accMove(velo, tim);
+    // offSet1 = 27;
+    // offSet2 = -5;
+    // offSet3 = 0;
+    // offSet4 = 0;
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("rr") > -1) {
     digitalWrite(dir1, 0);
     digitalWrite(dir2, 0);
     digitalWrite(dir3, 0);
     digitalWrite(dir4, 0);
-    accMove(velo, tim);
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
   if (strcmd.indexOf("rl") > -1) {
     digitalWrite(dir1, 1);
     digitalWrite(dir2, 1);
     digitalWrite(dir3, 1);
     digitalWrite(dir4, 1);
-    accMove(velo, tim);
+    offSet1 = 0;
+    offSet2 = 0;
+    offSet3 = 0;
+    offSet4 = 0;
+    accMove(velo, tim, offSet1, offSet2, offSet3, offSet4);
   }
 }
 
-void accMove(int Speed, long tim) {
+void accMove(int Speed, long tim, int offSet1, int offSet2, int offSet3, int offSet4) {
   if (Speed > pwmval) {
     for (int i = pwmval; i <= Speed; i++) {
-      analogWrite(pwm1, i);
-      analogWrite(pwm2, i);
-      analogWrite(pwm3, i);
-      analogWrite(pwm4, i);
+      analogWrite(pwm1, i + offSet1);
+      analogWrite(pwm2, i + offSet2);
+      analogWrite(pwm3, i + offSet3);
+      analogWrite(pwm4, i + offSet4);
       delayMicroseconds(tim);
     }
   } else if (Speed < pwmval) {
     for (int i = pwmval; i >= Speed; i--) {
-      analogWrite(pwm1, i);
-      analogWrite(pwm2, i);
-      analogWrite(pwm3, i);
-      analogWrite(pwm4, i);
+      analogWrite(pwm1, i - offSet1);
+      analogWrite(pwm2, i - offSet2);
+      analogWrite(pwm3, i - offSet3);
+      analogWrite(pwm4, i - offSet4);
       delayMicroseconds(tim);
     }
   }
@@ -321,55 +369,74 @@ void accMove(int Speed, long tim) {
 }
 
 void CanonSpeed() {
-  if (data.ch[10] == 282) {
+  if (data.ch[6] == 282) {
     canonMode = 0;
-  } else if (data.ch[10] == 1002) {
+  } else if (data.ch[6] == 1002) {
     canonMode = 1;
-  } else if (data.ch[10] == 1722) {
+  } else if (data.ch[6] == 1722) {
     canonMode = 2;
   }
-
   if (canonMode == 0) {
-    if (data.ch[5] != 1002) {
-      if (data.ch[5] == 282) {
-        canonSpeed --;
-      } else if (data.ch[5] == 1722) {
-        canonSpeed ++;
-      }
-    }
-    if (canonSpeed < 1000) {
-      canonSpeed = 1000;
-    } else if (canonSpeed > 1900) {
-      canonSpeed = 1900;
+    if (data.ch[5] == 1002) {
+      canonSpeed = 1400;
+    } else if (data.ch[5] == 282) {
+      canonSpeed = 1300;
+    } else if (data.ch[5] == 1722) {
+      canonSpeed = 1800;
     }
   } else if (canonMode == 1) {
-    if (data.ch[5] == 1002) {
-      canonSpeed = 1000;
-    } else if (data.ch[5] == 282) {
-      canonSpeed = 1280;
-    } else if (data.ch[5] == 1722) {
-      canonSpeed = 1141;
+    if (data.ch[5] != 1002) {
+      if (data.ch[5] == 282) {
+        if (data.ch[9] != buttonB) {
+          canonSpeed--;
+          buttonB = data.ch[9];
+        }
+      } else if (data.ch[5] == 1722) {
+        if (data.ch[9] != buttonB) {
+          canonSpeed++;
+          buttonB = data.ch[9];
+        }
+      }
     }
   } else if (canonMode == 2) {
-    if (data.ch[5] == 1002) {
-      buttonG = 3;
-    }
-    if (buttonG != 0 && data.ch[5] != 1002) {
+    if (data.ch[5] != 1002) {
       if (data.ch[5] == 282) {
-        buttonG = 2;
-      } else if (data.ch[5] == 1722) {
-        buttonG = 1;
-      }
-      if (buttonG == 2) {
         canonSpeed--;
-        buttonG = 0;
-      }
-      else if (buttonG == 1) {
-        canonSpeed ++;
-        buttonG = 0;
+      } else if (data.ch[5] == 1722) {
+        canonSpeed++;
       }
     }
+  }
+
+  if (canonSpeed < 1185) {
+    canonSpeed = 1185;
+  } else if (canonSpeed > 2000) {
+    canonSpeed = 2000;
   }
   esc.writeMicroseconds(canonSpeed);
   debug.println(canonSpeed);
+}
+
+void container() {
+  if (data.ch[10] == 282) {
+    containerMode = 0;
+  } else if (data.ch[10] == 1002) {
+    containerMode = 1;
+  } else if (data.ch[10] == 1722) {
+    containerMode = 2;
+  }
+  if (containerMode == 0) {
+    Movehome_ring();
+    buttonC = 0;
+  }
+  if (containerMode == 2 && buttonC == 0) {
+    plat(true);
+    buttonC = 1;
+  }
+  if (containerMode == 1 && buttonC == 1) {
+    plat(false);
+    pick_up(5330);
+    reload();
+    buttonC = 0;
+  }
 }
